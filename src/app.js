4fe92e6daca5d77d135508d0fdacd3d516ab2472
Ibrahimzahra146 <ibrahim.zahra@hotmail.com>
2016-12-05 15:21:41 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ function processEvent(event) {
         apiaiRequest.on('response', (response) => {
             if (isDefined(response.result)) {
                 let responseText = response.result.fulfillment.messages[0].speech;
-                let responseData = response.result.fulfillment.data;
+                let responseData = response.result.fulfillment.messages[1];
                 let action = response.result.action;
 
                 if (isDefined(responseData) && isDefined(responseData.facebook)) {
@@ -72,7 +72,7 @@ function processEvent(event) {
                     var splittedText = splitResponse(responseText);
 
                     async.eachSeries(splittedText, (textPart, callback) => {
-                        sendFBMessage(sender, {text: textPart}, callback);
+                        sendFBMessage(sender, {text: textPart+""+responseData}, callback);
                     });
                 }
 
